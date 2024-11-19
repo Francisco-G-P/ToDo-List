b733ebc738feac7b3f17c9598d444e06eba75f9d
Francisco G P <francisco.gonzalez@encora.com>
2024-11-19 16:37:56 -0700

    Front-end Load

---
+++ Front-end/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import TaskTable from './components/TaskTable';
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <TaskTable />
+    </div>
+  );
+};
+
+export default App;
